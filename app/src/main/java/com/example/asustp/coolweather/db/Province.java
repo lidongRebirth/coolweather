c8d0b6aef12ead4c9abc23bf3a406464a5719a69
LD <836354552@qq.com>
2018-07-02 18:55:57 +0800

    加入创建数据库和表的各种配置

---
+++ app/src/main/java/com/example/asustp/coolweather/db/Province.java
@@ -0,0 +1,33 @@
+package com.example.asustp.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;                         //每个实体类都应该有的字段
+    private String provinceName;    //记录省名字
+    private int provinceCode;       //记录省代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
