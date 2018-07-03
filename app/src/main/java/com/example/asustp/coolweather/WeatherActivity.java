187c448eafb8c31b8bac2855e8d4b4fdcb9fadde
LD <836354552@qq.com>
2018-07-03 10:28:19 +0800

    增加后天自动更新天气的功能

--- app/src/main/java/com/example/asustp/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/asustp/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.asustp.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.asustp.coolweather.gson.Forecast;
 import com.example.asustp.coolweather.gson.Weather;
+import com.example.asustp.coolweather.service.AutoUpdateService;
 import com.example.asustp.coolweather.util.HttpUtil;
 import com.example.asustp.coolweather.util.Utility;
 
@@ -221,6 +223,10 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
+
     }
 
     /**
